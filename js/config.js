c1f1a4bc5167efa0c56014c6f080c188b247231b
Training <training@ip-172-31-21-118.us-east-2.compute.internal>
2024-03-06 23:02:45 +0000

    invoke url

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://n1569t2fr5.execute-api.us-west-2.amazonaws.com/tfnn-Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
